f3896633b531e1b6168c5fb00faffd3bfff731e3
AjiYakin <ajiyakin91@gmail.com>
2025-08-19 10:01:42 +0700

    update php 5.6nix flake

--- php56/flake.nix
+++ php56/flake.nix
@@ -2,24 +2,24 @@
   description = "PHP 5.6 flake";
 
   inputs = {
-    # Pin to a nixpkgs commit from around 2019 where PHP 5.6 existed
+    # Use official release tarball instead of GitHub
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/2ee2f9dc0e3ce2066b1c1b6d0da7a58c3d5a8b03";
+      url = "https://releases.nixos.org/nixos/18.09/nixos-18.09.2574.4743347b179/nixexprs.tar.xz";
+      flake = false;
     };
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.${system}.php56 = pkgs.php56;
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.${system}.php56 = pkgs.php56;
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.php56
-          pkgs.php56Packages.composer
-        ];
-      };
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.php56
+        pkgs.php56Packages.composer
+      ];
     };
+  };
 }

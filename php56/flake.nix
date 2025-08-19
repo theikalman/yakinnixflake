3b38ecc09014c5fe7bdc7351649c6e250ce72409
AjiYakin <ajiyakin91@gmail.com>
2025-08-19 10:17:08 +0700

    update flake.nix for php 5.6

--- php56/flake.nix
+++ php56/flake.nix
@@ -1,25 +1,90 @@
 {
-  description = "PHP 5.6 flake";
+  description = "PHP 5.6 Development Environment";
 
   inputs = {
-    # Use official release tarball instead of GitHub
-    nixpkgs = {
-      url = "https://releases.nixos.org/nixos/18.09/nixos-18.09.2574.4743347b179/nixexprs.tar.xz";
-      flake = false;
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-shell.url = "github:loophp/nix-shell";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    packages.${system}.php56 = pkgs.php56;
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      systems,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
 
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.php56
-        pkgs.php56Packages.composer
-      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          lib,
+          ...
+        }:
+        let
+          php = pkgs.api.buildPhpFromComposer {
+            src = inputs.self;
+            php = pkgs.php56; # Change to php56, php70, ..., php81, php82, php83 etc.
+          };
+        in
+        {
+          _module.args.pkgs = import self.inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.nix-shell.overlays.default ];
+            config.allowUnfree = true;
+          };
+
+          devShells.default = pkgs.mkShellNoCC {
+            name = "php-devshell";
+            buildInputs = [
+              php
+              php.packages.composer
+              pkgs.phpunit
+            ];
+          };
+
+          apps = {
+            # nix run .#composer -- --version
+            composer = {
+              type = "app";
+              program = lib.getExe (
+                pkgs.writeShellApplication {
+                  name = "composer";
+
+                  runtimeInputs = [
+                    php
+                    php.packages.composer
+                  ];
+
+                  text = ''
+                    ${lib.getExe php.packages.composer} "$@"
+                  '';
+                }
+              );
+            };
+
+            # nix run .#phpunit -- --version
+            phpunit = {
+              type = "app";
+              program = lib.getExe (
+                pkgs.writeShellApplication {
+                  name = "phpunit";
+
+                  runtimeInputs = [ php ];
+
+                  text = ''
+                    ${lib.getExe pkgs.phpunit} "$@"
+                  '';
+                }
+              );
+            };
+          };
+        };
     };
-  };
 }
